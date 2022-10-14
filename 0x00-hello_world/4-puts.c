8672e1a5e37950b7ebd8eadb423f09902f8ff495
Sibusiso Dlamini <74040586+Innocent-Dlamini-3080@users.noreply.github.com>
2022-10-14 15:28:00 +0200

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main - Entry point This function just printd words
+ *  This function just printd words
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
